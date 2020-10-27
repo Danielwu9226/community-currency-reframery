fb2f3855d0b9795c53987c2d7aaebe5bb0428cd4
Charles Zhang <charlesdev99@gmail.com>
2020-10-27 17:12:13 -0400

    fix duplicate contract warning

--- eth/contracts/DANC.sol
+++ eth/contracts/DANC.sol
@@ -3,7 +3,6 @@ pragma solidity >=0.4.22 <0.8.0;
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
 contract DANC is ERC20{
-    constructor() ERC20("DANC", "DNC") public{
-
+    constructor(address owner_address) ERC20("DANC COIN", "DNC") public{
     }
 } 

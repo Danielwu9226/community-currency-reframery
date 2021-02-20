6479c47fccfc4568c002aeaf1bd3f01f12c74509
DanielDabouu9226 <Danielwu9226@gmail.com>
2021-02-20 14:38:56 -0500

    AWS S3 bucket - various detected image formats upload to s3 bucket
    s3 bucket under user_images/
    return object url after image is uploaded to s3 bucket.

---
+++ backend/s3_upload/s3_upload.py
@@ -0,0 +1,25 @@
+#author: Daniel Wu
+
+from secret import access_key, secret_key
+import boto3
+import os
+
+def s3():
+    client = boto3.client('s3',
+                        aws_access_key_id = access_key,
+                        aws_secret_access_key = secret_key)
+
+    for file in os.listdir():
+        if file.endswith(".png") or file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith(".PNG") or file.endswith(".JPG") or file.endswith(".JPEG"):
+             for image in file:
+                AWS_STORE_BUCKET_NAME = "reframery-image"
+                upload_file_key = 'user_images/' + str(file)
+                client.upload_file(file, AWS_STORE_BUCKET_NAME, upload_file_key)
+
+            url = (f'https://{AWS_STORE_BUCKET_NAME}.s3.amazonaws.com/{upload_file_key}')
+            
+    return url
+
+#test output
+# if __name__ == '__main__':
+#     print(s3())
---
+++ backend/s3_upload/secret.py
@@ -0,0 +1,2 @@
+access_key = "AKIAV7J575MZM76T5P7Z"
+secret_key = "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"
